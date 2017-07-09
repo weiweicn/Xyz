1b46b948a9cc5c59e0f76de9302de466ac0d6c8c
unknown <Administrator@LZ_SC__N.(none)>
2017-07-09 22:38:53 +0800

    Initial commit

---
+++ app/src/main/java/c/b/a/xyz/MainActivity.java
@@ -0,0 +1,14 @@
+package c.b.a.xyz;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
